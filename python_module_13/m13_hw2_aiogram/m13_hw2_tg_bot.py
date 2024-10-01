28173d890557a29821501ad1eb13b71bc055eeb3
Kristina <zabiryuchenko.kristina@gmail.com>
2024-10-01 14:47:07 +0300

    module 13 homework 5 inline keyboard

--- python_module_13/m13_hw2_aiogram/m13_hw2_tg_bot.py
+++ python_module_13/m13_hw2_aiogram/m13_hw2_tg_bot.py
@@ -3,6 +3,7 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.dispatcher import FSMContext
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import asyncio
 
 api = ''
@@ -13,10 +14,29 @@ kb = ReplyKeyboardMarkup()
 btn_calc = KeyboardButton(text='Рассчитать')
 btn_info = KeyboardButton(text='Информация')
 
-kb.add(btn_calc)
-kb.add(btn_info)
+kb.add(btn_calc, btn_info)
 
 kb.resize_keyboard = True
+
+kb_inline = InlineKeyboardMarkup()
+
+btn1 = InlineKeyboardButton(text='Рассчитать норму калорий', callback_data='calories')
+btn2 = InlineKeyboardButton(text='Формулы расчёта', callback_data='formulas')
+
+kb_inline.add(btn1, btn2)
+
+kb_inline.resize_keyboard = True
+
+
+@dp.message_handler(text=['Рассчитать'])
+async def main_menu(message):
+	await message.answer('Выберите опцию:', reply_markup=kb_inline)
+
+
+@dp.callback_query_handler(text='formulas')
+async def get_formulas(call):
+	await call.message.answer('10 x вес (кг) + 6,25 x рост (см) – 5 x возраст (г) – 161')
+	await call.answer()
 
 
 class UserState(StatesGroup):
@@ -30,10 +50,11 @@ async def start(message):
 	await message.answer('Привет! Я бот помогающий твоему здоровью.', reply_markup=kb)
 
 
-@dp.message_handler(text=['Рассчитать'])
-async def set_age(message):
-	await message.answer('Введите свой возраст:')
+@dp.callback_query_handler(text=['calories'])
+async def set_age(call):
+	await call.message.answer('Введите свой возраст:')
 	await UserState.age.set()
+	await call.answer()
 
 
 @dp.message_handler(state=UserState.age)
