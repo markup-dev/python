9b7a14c6ff9450b002e612df127b727fd122802d
Kristina <zabiryuchenko.kristina@gmail.com>
2024-11-08 16:43:11 +0300

    module 16 homework 4 Pydantic

--- python_module_16/m16_hw3_crud_requests/main.py
+++ python_module_16/m16_hw3_crud_requests/main.py
@@ -1,13 +1,20 @@
-from fastapi import FastAPI, Path
-from typing import Annotated
+from fastapi import FastAPI, Path, HTTPException
+from typing import Annotated, List
+from pydantic import BaseModel
 
 app = FastAPI()
 
-users = {'1': 'Имя: Example, возраст: 18'}
+users = []
+
+
+class User(BaseModel):
+	id: int = None
+	username: str = None
+	age: int = None
 
 
 @app.get('/users/')
-async def get_all_users() -> dict:
+async def get_all_users() -> List[User]:
 	return users
 
 
@@ -15,8 +22,9 @@ async def get_all_users() -> dict:
 async def post_user(
 		username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username', example='UrbanUser')],
 		age: Annotated[int, Path(gt=18, le=120, description='Enter age', example=22)]) -> str:
-	new_user_id = str(int(max(users, key=int)) + 1)
-	users[new_user_id] = f'Имя: {username}, возраст: {age}'
+	new_user_id = len(users) + 1
+	user = User(id=new_user_id, username=username, age=age)
+	users.append(user)
 	return f'User {new_user_id} is registered'
 
 
@@ -25,11 +33,19 @@ async def update_user(
 		user_id: Annotated[str, Path(description='Enter user id', example='1')],
 		username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username', example='UrbanUser')],
 		age: Annotated[int, Path(gt=18, le=120, description='Enter age', example=22)]) -> str:
-	users[user_id] = f'Имя: {username}, возраст: {age}'
-	return f'User {user_id} has been updated'
+	try:
+		edit_user = users[int(user_id)-1]
+		edit_user.username = username
+		edit_user.age = age
+		return f'User {user_id} has been updated'
+	except IndexError:
+		raise HTTPException(status_code=404, detail="User was not found")
 
 
 @app.delete('/user/{user_id}')
-async def delete_user(user_id: Annotated[str, Path(description='Enter user id', example='1')]) -> str:
-	users.pop(user_id)
-	return f'User {user_id} has been deleted'
+async def delete_user(user_id: Annotated[int, Path(description='Enter user id', example='1')]) -> object:
+	try:
+		deleted_user = users.pop(int(user_id)-1)
+		return deleted_user
+	except IndexError:
+		raise HTTPException(status_code=404, detail="User was not found")
