81212c83303f27a7518d117abadb00055498a1f2
Kristina <zabiryuchenko.kristina@gmail.com>
2024-11-08 19:58:53 +0300

    module 16 homework 5 Jinja2

--- python_module_16/m16_hw3_crud_requests/main.py
+++ python_module_16/m16_hw3_crud_requests/main.py
@@ -1,8 +1,12 @@
-from fastapi import FastAPI, Path, HTTPException
+from fastapi import FastAPI, Body, status, HTTPException, Request, Path
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 from typing import Annotated, List
 from pydantic import BaseModel
 
 app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
 
 users = []
 
@@ -13,19 +17,24 @@ class User(BaseModel):
 	age: int = None
 
 
-@app.get('/users/')
-async def get_all_users() -> List[User]:
-	return users
+@app.get('/')
+async def get_main_page(request: Request) -> HTMLResponse:
+	return templates.TemplateResponse('users.html', context={'request': request, 'users': users})
+
+
+@app.get('/user/{user_id}')
+async def get_users(request: Request, user_id: int) -> HTMLResponse:
+	return templates.TemplateResponse('users.html', context={'request': request, 'user': users[user_id-1]})
 
 
 @app.post('/user/{username}/{age}')
 async def post_user(
 		username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username', example='UrbanUser')],
 		age: Annotated[int, Path(gt=18, le=120, description='Enter age', example=22)]) -> str:
-	new_user_id = len(users) + 1
-	user = User(id=new_user_id, username=username, age=age)
+	user_id = len(users) + 1
+	user = User(id=user_id, username=username, age=age)
 	users.append(user)
-	return f'User {new_user_id} is registered'
+	return f'User {user_id} is registered'
 
 
 @app.put('/user/{user_id}/{username}/{age}')
@@ -34,7 +43,7 @@ async def update_user(
 		username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username', example='UrbanUser')],
 		age: Annotated[int, Path(gt=18, le=120, description='Enter age', example=22)]) -> str:
 	try:
-		edit_user = users[int(user_id)-1]
+		edit_user = users[int(user_id) - 1]
 		edit_user.username = username
 		edit_user.age = age
 		return f'User {user_id} has been updated'
@@ -45,7 +54,7 @@ async def update_user(
 @app.delete('/user/{user_id}')
 async def delete_user(user_id: Annotated[int, Path(description='Enter user id', example='1')]) -> object:
 	try:
-		deleted_user = users.pop(int(user_id)-1)
+		deleted_user = users.pop(int(user_id) - 1)
 		return deleted_user
 	except IndexError:
 		raise HTTPException(status_code=404, detail="User was not found")
